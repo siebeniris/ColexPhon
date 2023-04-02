762fb16d015b4f433bf2b76a29da17d707d6ed89
yiyi chen <chen.yiyi@protonmail.com>
2023-04-02 23:35:09 +0200

    get colex langs

--- src/preprocessing/get_colex_langs.py
+++ src/preprocessing/get_colex_langs.py
@@ -4,24 +4,28 @@ import json
 import pandas as pd
 
 
-def get_synsets_langs(inputfolder="~/LangSim/data/synsets_bb_lang", outputfolder="data/preprocessed/synsets"):
+def get_synsets_langs(inputfolder="~/LangSim/data/linguistic_features/langdfs",
+                      outputfolder="data/preprocessed/synsets"):
     with open("data/preprocessed/EN_synsets.json") as f:
         synsets = json.load(f)
 
     for file in os.listdir(inputfolder):
         filepath = os.path.join(inputfolder, file)
         writepath = os.path.join(outputfolder, file)
-        writer = open(writepath, "w+")
+
         print(f"processing file {filepath}")
-        with open(filepath) as f:
 
-            for line in f.readlines():
-                t1, t2 = line.replace("\n", "").split("\t")
-                if t1 in synsets:
-                    writer.write(line)
+        df = pd.read_csv(filepath)
+        df[["SYN1", "SYN2"]] = df["COLEX"].str.split("_", expand=True)
+        print(f"len df {len(df)}")
 
-        writer.close()
+        df_syn = df[(df["SYN1"].isin(synsets)) | (df["SYN2"].isin(synsets))]
+        print(f"len df syn {len(df_syn)}")
+
+        df_syn.to_csv(writepath, index=False)
+
 
 if __name__ == '__main__':
     import plac
+
     plac.call(get_synsets_langs)
